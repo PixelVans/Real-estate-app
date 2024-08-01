f20635011cf0e62d4f58d049e66674a8f60622dc
PixelVans <evansmemba001@gmail.com>
2024-08-02 00:36:57 +0300

    Routes Definations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import Home from './assets/pages/Home'
+import SignIn from './assets/pages/SignIn'
+import SignUp from './assets/pages/SignUp'
+import About from './assets/pages/About'
+import Profile from './assets/pages/Profile'
 export default function App() {
 
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/sign-in" element={<SignIn/>}></Route>
+        <Route path="/sign-up" element={<SignUp/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
+      </Routes>
+    </BrowserRouter>
     
   )
 }
