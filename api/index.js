0b33997d8bba61df5a46d3655367c0070974603f
PixelVans <evansmemba001@gmail.com>
2024-08-02 22:49:21 +0300

    user model created

--- api/index.js
+++ api/index.js
@@ -1,8 +1,30 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
 const app = express();
+import dotenv from 'dotenv';
 
-app.listen(3000, () => {
+dotenv.config();
+
+
+
+
+const Port = process.env.PORT || 3000
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.error('Failed to connect to MongoDB', error));
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(Port, () => {
     console.log('server is listening on port 3000')
 })
 
