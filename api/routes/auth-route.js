7f456ef6fd59f4b6723ad1786fb64ba38759747c
PixelVans <evansmemba001@gmail.com>
2024-08-03 18:46:07 +0300

    sign in user and authentication created

--- api/routes/auth-route.js
+++ api/routes/auth-route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import {signup} from '../controllers/auth-controller.js'
+import {signup,signin} from '../controllers/auth-controller.js'
 
 router.post('/sign-up', signup)
+router.post('/sign-in', signin)
 
 
 export default router
