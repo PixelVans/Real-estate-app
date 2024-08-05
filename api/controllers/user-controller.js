5939d6fbf51522a28fe27499aeb23f2b9f7f300c
PixelVans <evansmemba001@gmail.com>
2024-08-05 21:54:26 +0300

    delete user and token functionality

--- api/controllers/user-controller.js
+++ api/controllers/user-controller.js
@@ -11,6 +11,7 @@ export const test = (req, res) => {
 }
 
 
+
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id) return next(errorHandler(
         401,'You can only update your own account'
@@ -36,3 +37,21 @@ export const updateUser = async (req, res, next) => {
        
     }
 }
+
+
+
+
+export const  deleteUser = async (req,res,next) => {
+  if(req.user.id !== req.params.id) return next(errorHandler(401,'You can only delete your own account'))
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.clearCookie('access_token')
+        res.status(200).json('User has been deleted')
+    }
+    catch (error) {
+        next(error)
+    }
+
+}
+
+
