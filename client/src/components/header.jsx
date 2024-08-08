f651502ff29f9e91515da4bd8c1d2c56802a9cbb
PixelVans <evansmemba001@gmail.com>
2024-08-09 00:08:26 +0300

    updated header search form

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
-import {Link} from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import {Link,useNavigate} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+
+
 
 
 export default function Header() {
-  const {currentUser} = useSelector(state=>state.user)
+  const { currentUser } = useSelector(state => state.user)
+  const [searchTerm, setSearchTerm] = useState('')
+  const navigate = useNavigate();
+  
+
+  //HANDLE SUBMIT
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
       <header className='bg-slate-200 shadow-md'>
           <div className='flex justify-between items-center 
@@ -16,11 +52,24 @@ export default function Header() {
          <span className='text-slate-700'>Estate</span>
         </h1> 
         </Link>
-          <form action="" className='bg-slate-100 p-3 rounded-lg flex items-center'>
-          <input type="text" placeholder='Search...'
+
+        
+        <form
+          onSubmit={handleSubmit}
+          className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input
+            type="text"
+            placeholder='Search...'
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
             className='bg-transparent focus:outline-none w-24 sm:w-64' />
-          <FaSearch className="text-slate-600"/>
+          <button>
+           <FaSearch className="text-slate-600" /> 
+          </button>
+          
         </form> 
+
+
         <ul className='flex gap-4'>
           <Link to='/'>
           <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
