e88ca944b881a70df863bb7899d53139d61503c0
PixelVans <evansmemba001@gmail.com>
2024-08-10 00:49:39 +0300

    fixed signUp

--- api/controllers/auth-controller.js
+++ api/controllers/auth-controller.js
@@ -4,7 +4,11 @@ import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
-  const { username, email, password } = req.body
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: 'Username, email, and password are required.' });
+  }
   const hashedPassword = bcryptjs.hashSync(password, 10)
   const newUser = new User({ username, email, password: hashedPassword });
     
